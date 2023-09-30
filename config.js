62608d1a1392930691c44b8871fa75b32a7b7e25
lyreneliu <lyreneliu@gmail.com>
2023-09-30 18:16:47 +0800

    feat: config file

---
+++ config.js
@@ -0,0 +1,19 @@
+const custom = {
+    translateInNewWindow: true,
+    translateFrom: 'en',
+    translateTo: 'fr'
+};
+
+const googleTranslateUrl = 'https://translate.google.com/?op=images';
+
+const msgs = {
+    picCopied: 'Picture copied.',
+    transPageActive: 'Google Translate is active.',
+    tabInit: 'Tab is initialized.'
+};
+
+export {
+    custom,
+    googleTranslateUrl,
+    msgs
+};
