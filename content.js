ba07cd0c7540b7aaed1e2af2136a75d8c1319f18
lyreneliu <lyreneliu@gmail.com>
2023-10-01 01:50:42 +0800

    fix: open windows duplicated

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 let config;
-let contentmenuInterval = null;
+let contentmenuTimeout = null;
 
 async function init() {
     await getConfig();
@@ -12,11 +12,17 @@ function onLoad() {
     observeBtn();
     document.addEventListener('copy', callTransTab);
     document.addEventListener('contextmenu', async function (e) {
-        await navigator.clipboard.writeText('');
-        contentmenuInterval = setInterval(async function () {
-            if (await navigator.clipboard.read()) callTransTab();
-        }, 500);
+        clearTimeout(contentmenuTimeout);
+        await clearClipboard();
+        contentmenuTimeout = setTimeout(onClipboardChanged, 500);
     });
+}
+
+async function onClipboardChanged() {
+    let current = await getCopied() || null;
+    if (!!current) current = await clipboard2Blob(current);
+    if (!!current) return callTransTab();
+    contentmenuTimeout = setTimeout(onClipboardChanged, 500);
 }
 
 function observeBtn() {
@@ -33,11 +39,11 @@ function observeBtn() {
 }
 
 async function callTransTab() {
+    clearTimeout(contentmenuTimeout);
     try {
-        clearInterval(contentmenuInterval);
-        const COPIED = await navigator.clipboard.read();
-        for (let i = 0, l = COPIED.length; i < l; i ++) {
-            if (!COPIED[i].types.find((v) => v.match('image'))) return;
+        const data = getCopied(true);
+        for (let i = 0, l = data.length; i < l; i ++) {
+            if (!data[i].types.find((v) => v.match('image'))) return;
         }
         chrome.runtime.sendMessage(config.msgs.picCopied);
     } catch (e) { // if Chrome haven't got the clipboard permission
@@ -46,18 +52,15 @@ async function callTransTab() {
 }
 
 function addFocusListener(func) {
-    let focus = window.addEventListener('focus', function () {
+    let focus = function () {
         window.removeEventListener('focus', focus);
         func();
-    });
+    };
+    window.addEventListener('focus', focus);
 }
 
 function pastePic() {
     getBtn().click();
-}
-
-function getBtn() {
-    return document.querySelector(`[aria-label="${config.custom.btnAriaLabel}"]`);
 }
 
 function getConfig() {
@@ -71,6 +74,49 @@ function getConfig() {
         chrome.runtime.onMessage.addListener(setConfig);
         chrome.runtime.sendMessage('config');    
     });
+}
+
+function getBtn() {
+    return document.querySelector(`[aria-label="${config.custom.btnAriaLabel}"]`);
+}
+
+async function hasPermission() {
+    const permission = await navigator.permissions.query({
+        name: 'clipboard-read',
+        allowWithoutGesture: false
+    });
+    return permission.state === 'granted';
+}
+
+
+async function getCopied(throwing = false) {
+    if (!await hasPermission()) return;
+    try {
+        return await navigator.clipboard.read();
+    } catch (e) {
+        if (!throwing) return;
+        throw Error(e);
+    }
+}
+
+async function clipboard2Blob(data) {
+    let text = '';
+    for (let i = 0, il = data.length; i < il; i ++) {
+        for (let j = 0, jl = data[i].types.length; j < jl; j ++) {
+            const blob = await data[i].getType(data[i].types[j]);
+            text += await blob.text();
+        }    
+    }
+    return text;
+}
+
+async function clearClipboard() {
+    if (!await hasPermission()) return;
+    let item = new ClipboardItem({
+        'text/plain': new Blob([''], { type: 'text/plain' })
+    });
+    await navigator.clipboard.write([item]);
+    return '';
 }
 
 init();
